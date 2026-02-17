d08ddb054d40763eab8ea40ac111699f53ac0600
Francesco Coradeschi <kora@Air-di-Kora.local>
2026-02-17 18:31:35 +0100

    Revert "big commit"
    This reverts commit 55eb05c88b31a88f4b10f799b7a8e4a2e9169998.

--- tester/mock_service.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import random
-
-
-app = FastAPI()
-
-@app.get("/status/{job_id}")
-def status(job_id: str):
-    return {"status": random.choice(["pending", "running", "done"])}
