8eba4ae6d403aa4c3b30f51002cb3a8cf4b98ae8
Francesco Coradeschi <kora@Air-di-Kora.local>
2026-03-27 17:08:32 +0100

    fixed parsing of service parameters

--- observer/scheduler/db_interface/services_interface.py
+++ observer/scheduler/db_interface/services_interface.py
@@ -1,3 +1,4 @@
+import logging
 
 from observer.config import GLOBAL_PSEUDOSERVICE
 from observer.config import DUMMY_SERVICE
@@ -5,6 +6,8 @@ from observer.scheduler.poll_logic.service import Service
 from observer.config import DEFAULT_SERVICE_TIMEOUT, DEFAULT_SERVICE_MAX_CONCURRENCY
 from observer.api.db_interface.api_db_interface import get_db as get_api_db
 
+
+logger = logging.getLogger(__name__)
 
 RESERVED_NAMES = [GLOBAL_PSEUDOSERVICE, DUMMY_SERVICE]
 
@@ -23,12 +26,29 @@ def import_services():
             query_params=cfg.get("query_params", {}),
             body=cfg.get("body", {}),
             static_headers=cfg.get("headers", {}),
-            timeout=cfg.get("timeout", DEFAULT_SERVICE_TIMEOUT),
+            timeout=get_float(name, cfg, "timeout", DEFAULT_SERVICE_TIMEOUT),
             status_field=cfg.get("status_field", "status"),
             terminal_states=set(cfg.get("terminal_states", [])),
-            max_concurrency=cfg.get("max_concurrency", DEFAULT_SERVICE_MAX_CONCURRENCY),
+            max_concurrency=get_int(name, cfg, "max_concurrency", DEFAULT_SERVICE_MAX_CONCURRENCY),
             auth_type=cfg.get('auth_type'),
             auth_config=cfg.get('auth_config', {})
         )
 
     return services
+
+
+def get_int(service_name, config_dict, key, default):
+    value = config_dict.get(key)
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        logger.warning(f'Using default value for unparsable {key} in {service_name}, value was {value}')
+        return default
+
+def get_float(service_name, config_dict, key, default):
+    value = config_dict.get(key)
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        logger.warning(f'Using default value for unparsable {key} in {service_name}, value was {value}')
+        return default
